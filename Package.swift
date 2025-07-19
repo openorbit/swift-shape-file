77ff75211a3047bfa1874f5088302db6d2acd2c2
Mattias Holm <lorrden@openorbit.org>
2025-07-19 07:27:22 +0200

    Rename product

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-shapefile",
+            name: "Shapefile",
             targets: ["Shapefile"]
         ),
     ],
